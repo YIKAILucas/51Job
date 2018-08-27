9ffeb9264ad5a59ed9f046090b30134a5f928658
vimeracke <vimer757216574@gmail.com>
2018-08-27 19:25:12 +0800

    采集数据完成

--- crawel_utils/download.py
+++ crawel_utils/download.py
@@ -40,36 +40,31 @@ USER_AGENT_LIST = [
 
 
 class Download(object):
-    url = ''
+    db_host = '127.0.0.1'
     db_name = '前程无忧'
     set_home = '首页信息'
     set_details = '详情页'
+    set_download = '下载队列'
     sleep_time = 1
     proxies = ()
 
     def __init__(self):
         pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
         self.r = redis.Redis(connection_pool=pool)
-        self.set = mongo_util.get_collection(self.db_name, self.set_home)
+        self.col_download = mongo_util.get_collection(self.db_name, self.set_download, self.db_host)
+        self.col_home = mongo_util.get_collection(self.db_name, self.set_home, self.db_host)
+        self.col_details = mongo_util.get_collection(self.db_name, self.set_details, self.db_host)
 
-    def set_url(self, set, laguage, page):
-        url = 'https://search.51job.com/list/030500,000000,0000,00,9,99,' \
-              + laguage + ',2,' \
-              + str(page) \
-              + '.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
-        dic = {'url': url, 'page': page, 'laguage': laguage, 'status': 'ready'}
-        set.insert(dic)
-        # self.r.hset('url', page, dic)
+    def get_url(self):
+        pass
 
-    def get_url(self, set):
-        return set.find_one_and_delete({})
-        # url = self.r.hget('url', page)
-        # url = eval(url)
-        # url = url['url']
-        # return url['url']
+    # x = self.col_download.find_and_modify(query={'status': {$gt: 'ready'}}, update = { $set: {
+    #     'status': 'downloading'}})
+    # if x['url'] and x['laguage'] is not None:
+    #     return x['url'], x['laguage']
 
     def get_list_url(self, laguage):
-        col = mongo_util.get_collection('下载队列', laguage)
+        col = mongo_util.get_collection(self.db_name, laguage)
         urls = col.find()
         return list(urls)
 
@@ -79,8 +74,6 @@ class Download(object):
             print('Downloading', url)
             response = requests.get(url, headers=headers, proxies=proxies)
             html = response.content.decode('gbk')
-            # html=response.text.encode('iso-8859-1').decode('gbk')
-
         except requests.exceptions.ConnectionError as e:
             print('Download error:', e.errno)
             html = None
@@ -89,7 +82,7 @@ class Download(object):
                     return self.downloading(url, num_retries - 1)
         return html
 
-    def parse(self, html):
+    def parse_one_page(self, html):
         h = etree.HTML(html)
         el = h.xpath('.//div[@class="dw_wp"]/div[@id="resultList"]/div[@class="el"]')
         result = []
@@ -101,84 +94,87 @@ class Download(object):
                 situation = i.xpath('./span[@class="t3"]/text()')[0]
                 salary = i.xpath('./span[@class="t4"]/text()')[0]
                 publish_time = i.xpath('./span[@class="t5"]/text()')[0]
-
                 dic = {'company': str(company), 'job': job, 'situation': situation, 'salary': salary,
                        'detail_url': detail_url, 'publish_time': publish_time}
-                result.append(dic)
+                if dic is not None:
+                    result.append(dic)
             except IndexError:
                 continue
             finally:
                 pass
         return result
 
-    def persistence(self, laguage, lis):
-        set = mongo_util.get_collection('前程无忧', laguage)
-        set.insert_many(lis)
+    def persistence(self, lis):
+        if lis is not None and len(lis) != 0:
+            self.col_home.insert_many(lis)
 
-    def all_process(self, laguage):
-        col = mongo_util.get_collection('下载队列',laguage)
+    def all_process(self, download_dic):
         try:
-            urls = d.get_list_url(laguage)
+            url, laguage = download_dic['url'], download_dic['laguage']
         except TypeError:
             return
-
-        html = None
-        for i in urls:
-            url = i['url']
-            if url is not None:
-                html = self.downloading(url=url)
-            else:
-                result = self.parse(html)
-                if result is not None:
-                    self.persistence(laguage, result)
+        if url is not None:
+            html = self.downloading(url=url)
+            result = self.parse_one_page(html)
+            for i in result:
+                i['laguage'] = laguage
+            if result is not None:
+                self.persistence(result)
 
     def multi_thread(self):
-        laguages = ['java', 'c++', '嵌入式', 'go', 'python', 'c#']
-
-        lis=[]
-        x = 0
-        for i in laguages:
-            lis.append(laguages[x])
-            x+=1
-        pool = threadpool.ThreadPool(6)
-        request = threadpool.makeRequests(self.all_process, laguages)
+        que = Queue()
+        urls = list(self.col_download.find())
+        for i in urls:
+            try:
+                que.put({'url': i['url'], 'laguage': i['laguage']})
+            except:
+                continue
+        x = que.qsize()
+        pool = threadpool.ThreadPool(x)
+        arg_group = [que.get() for i in range(x)]
+        request = threadpool.makeRequests(self.all_process, arg_group)
         for req in request:
             pool.putRequest(req)
+            time.sleep(0.5)
         pool.wait()
 
     def multi_process(self):
         laguages = ['java', 'c++', '嵌入式', 'go', 'python', 'c#']
-
         p = Pool(6)
         for i in range(6):
             laguage = laguages[i]
-            p.apply_async(self.all_process, args=([laguage,],))
+            p.apply_async(self.all_process, args=([laguage, ],))
         p.close()
         p.join()
 
 
+# def test_back_url():
+#     d = Download()
+#     d.col_download.find_and_modify()
+#     d.col_download.find_one_and_update()
+#
 #
 # def test_producer():
+#     d = Download()
 #     laguages = ['java', 'c++', 'go', 'python', 'c#']
-#
+#     #
 #     # laguages = ['嵌入式']
-#     index =0
 #     for l in laguages:
-#         laguage = laguages[index]
-#         set = mongo_util.get_collection('下载队列',laguage)
-#         index+=1
+#         laguage = l
+#         page = 1
 #         for i in range(1, 14):
 #             url = 'https://search.51job.com/list/030500,000000,0000,00,9,99,' \
 #                   + laguage + ',2,' \
-#                   + str(index) \
+#                   + str(page) \
 #                   + '.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
-#             # url = 'https://search.51job.com/list/030500,000000,0000,00,9,99,%25E5%25B5%258C%25E5%2585%25A5%25E5%25BC%258F,2,'+str(i)+'.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
+#             # url = 'https://search.51job.com/list/030500,000000,0000,00,9,99,%25E5%25B5%258C%25E5%2585%25A5%25E5%25BC%258F,2,'+str(page)+'.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
 #             dic = {'url': url, 'page': i, 'laguage': laguage, 'status': 'ready'}
-#             set.insert(dic)
+#             d.col_download.insert(dic)
+#             page+=1
+
 
 if __name__ == '__main__':
     laguage = 'java'
-    url_set = mongo_util.get_collection('下载队列', laguage)
     d = Download()
     # d.multi_process()
     d.multi_thread()
